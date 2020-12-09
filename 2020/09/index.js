822ba1f2c1dd8aa0c6088bfc9021aa243219cb13
Evaldas Lavrinovičius <lavrinovicius.evaldas@gmail.com>
2020-12-09 11:45:38 +0200

    Day 9 2020

---
+++ 2020/09/index.js
@@ -0,0 +1,56 @@
+const fs = require('fs');
+const assert = require('assert');
+
+const file = fs.readFileSync('./input.txt', 'utf8').split('\r\n').map(Number);
+
+const Part1 = (input, preamble = 25) => {
+  for (let i = preamble; i < input.length; i++) {
+    if (!contains(input[i], new Set(input.slice(i - preamble, i)))) {
+      return input[i];
+    }
+  }
+};
+
+const Part2 = (input, preamble = 25) => {
+  for (let i = preamble; i < input.length; i++) {
+    if (!contains(input[i], new Set(input.slice(i - preamble, i)))) {
+      return contiguous(input, input[i]);
+    }
+  }
+};
+
+const contiguous = (numbers, sum) => {
+  for (let i = 0; i < numbers.length; i++) {
+    let total = numbers[i];
+
+    for (let j = i + 1; j < numbers.length; j++) {
+      const subTotal = total + numbers[j];
+
+      if (subTotal > sum) {
+        break;
+      } else if (subTotal === sum) {
+        const range = numbers.slice(i, j);
+
+        return Math.min(...range) + Math.max(...range);
+      } else {
+        total += numbers[j];
+      }
+    }
+  }
+};
+
+const contains = (sum, range) => {
+  return [...range.values()].some((number) => range.has(sum - number));
+};
+
+console.log(`Part 1: ${Part1(file)}`); // 400480901
+console.log(`Part 2: ${Part2(file)}`); // 67587168
+
+// Test data
+const testData = fs.readFileSync('./testInput.txt', 'utf8').split('\r\n').map(Number);
+
+// Part 1 tests
+assert.strictEqual(Part1(testData, 5), 127);
+
+// Part 2 tests
+assert.strictEqual(Part2(testData, 5), 62);
