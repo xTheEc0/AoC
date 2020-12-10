9fb685a5d65f5569a1864a6804ce4ee0e5b12dfd
Evaldas Lavrinovičius <lavrinovicius.evaldas@gmail.com>
2020-12-10 19:09:04 +0200

    Day 10 2020

---
+++ 2020/10/index.js
@@ -0,0 +1,64 @@
+const fs = require('fs');
+const assert = require('assert');
+
+const file = fs
+  .readFileSync('./input.txt', 'utf8')
+  .split('\r\n')
+  .map(Number)
+  .sort((a, b) => a - b);
+
+const Part1 = (input) => {
+  const jolts = input.reduce(
+    (acc, jolt, idx, all) => {
+      const prev = all[idx - 1];
+      const diff = jolt - (prev || 0);
+      acc[diff] += 1;
+      return acc;
+    },
+    { 1: 0, 3: 1 }
+  );
+  return jolts['1'] * jolts['3'];
+};
+
+const Part2 = (input) => {
+  return input
+    .reduce(
+      (computed, jolt) => {
+        computed[jolt] = (computed[jolt - 3] || 0) + (computed[jolt - 2] || 0) + (computed[jolt - 1] || 0);
+        return computed;
+      },
+      [1]
+    )
+    .pop();
+};
+console.log(`Starting 'Part 1' Timer`);
+console.time('Part 1');
+console.log(`Part 1: ${Part1(file)}`); // 1984
+console.timeEnd('Part 1');
+
+console.log();
+
+console.log(`Starting 'Part 2' Timer`);
+console.time('Part 2');
+console.log(`Part 2: ${Part2(file)}`); // 3543369523456
+console.timeEnd('Part 2');
+
+// Test data
+const testData1 = fs
+  .readFileSync('./testInput1.txt', 'utf8')
+  .split('\r\n')
+  .map(Number)
+  .sort((a, b) => a - b);
+
+const testData2 = fs
+  .readFileSync('./testInput2.txt', 'utf8')
+  .split('\r\n')
+  .map(Number)
+  .sort((a, b) => a - b);
+
+// Part 1 tests
+assert.strictEqual(Part1(testData1), 35);
+assert.strictEqual(Part1(testData2), 220);
+// Part 2 tests
+assert.strictEqual(Part2(testData1), 8);
+assert.strictEqual(Part2(testData2), 19208);
